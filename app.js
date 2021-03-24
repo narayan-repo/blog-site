db92775a1b4e9ee883002177c3fc8a7063017385
narayan-repo <https://github.com/narayan-repo>
2021-03-24 22:26:24 +0530

    add database connection and set up HOME and NewPost routes

---
+++ app.js
@@ -0,0 +1,57 @@
+const express = require('express'),
+    bodyParser = require('body-parser'),
+    app = express(),
+    mongoose = require('mongoose');
+
+app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({extended: true}));
+
+//database connection
+mongoose.connect("mongodb://localhost:27017/blog_site", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+});
+
+const blog = new mongoose.Schema(
+    {
+        name: String,
+        image: String,
+        time: String,
+        description: String
+    }
+);
+
+const Blog = mongoose.model("Blog", blog);
+
+
+app.get("/", (req, res) => {
+    res.send("This is the root route");
+})
+
+app.get('/blogs', (req, res) => {
+    Blog.find({}, (err, blogs) => {
+        if (err) console.log(err)
+        else res.render('index', {blogs: blogs})
+    })
+})
+
+app.post('/blogs', (req, res) => {
+    console.log(req.body)
+    Blog.create(
+        {
+            name: req.body.name,
+            image: req.body.image,
+            time: req.body.time,
+            description: req.body.description
+        }
+    )
+    res.redirect('/blogs')
+})
+
+app.get('/blogs/new', (req, res) => {
+    res.render('new')
+})
+
+app.listen(3001, () => {
+    console.log("SERVER STARTED")
+})
